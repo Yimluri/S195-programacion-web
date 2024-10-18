3bbb07e71c52b30b2e14a9c4ca7bee07f41acd8c
Yimluri <jlugoarr04@gmail.com>
2024-10-18 17:52:44 -0600

    vista home

---
+++ practicaRepasos/resources/views/operaciones.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversiones</title>
+</head>
+<body>
+    <div>
+        <h2>Jimena Lugo Arriola</h2>
+        <h3></h3>
+    </div>
+</body>
+</html>
