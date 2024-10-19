0e7daed2f00ea7d893cf5d639f7351910fa0515c
Yimluri <jlugoarr04@gmail.com>
2024-10-18 18:46:30 -0600

    Update operaciones.blade.php

--- practicaRepasos/resources/views/operaciones.blade.php
+++ practicaRepasos/resources/views/operaciones.blade.php
@@ -12,25 +12,21 @@
             <label>Convertir de MB a GB</label>
             <input placeholder="Ingresa una cantidad de MG "></input>
             <button onclick="C1()">Convertir</button>
-            <p id="Resultado 1"></p>
             </div>
             <div>
             <label>Convertir de GB a MB</label>
             <input placeholder="Ingresa una cantidad de GB "></input>
             <button onclick="C2()">Convertir</button>
-            <p id="Resultado 2"></p>
             </div>
             <div>
             <label>Convertir de TB a GB</label>
             <input placeholder="Ingresa una cantidad de TB "></input>
             <button onclick="C3()">Convertir</button>
-            <p id="Resultado 3"></p>
             </div>
             <div>
             <label>Convertir de GB a TB</label>
             <input placeholder="Ingresa una cantidad de GB "></input>
             <button onclick="C4()">Convertir</button>
-            <p id="Resultado 4"></p>
             </div>
         </form>
     </div>
