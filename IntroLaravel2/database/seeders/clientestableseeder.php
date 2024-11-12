7c813cd47ba124c0b8141b2012a7b998f966efd3
Yimluri <jlugoarr04@gmail.com>
2024-11-11 19:01:59 -0600

    practica 7 parte 1

---
+++ IntroLaravel2/database/seeders/clientestableseeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class clientestableseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('clientes')->insert([
+            [
+                'nombre'=>'Jimena',
+                'apellido'=>'Lugo',
+                'correo'=>'jlugo@gmail.com',
+                'telefono'=>'4456789013'
+            ],
+            [
+                'nombre'=>'Uriel',
+                'apellido'=>'Martínez',
+                'correo'=>'UMhshgyu@gmail.com',
+                'telefono'=>'44567839677'
+            ],
+            [
+                'nombre'=>'Ana',
+                'apellido'=>'Ibarra',
+                'correo'=>'ana@gmail.com',
+                'telefono'=>'4456789097'
+            ]]);
+    }
+}
